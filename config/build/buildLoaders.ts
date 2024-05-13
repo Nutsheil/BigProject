46805e1f710476c66619c54e2d86a06d15d83de4
Nutshell <nutshell2088@yandex.ru>
2024-05-14 01:28:13 +0300

    connect react and scss loader in webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,13 +1,26 @@
 import { RuleSetRule } from 'webpack'
 
 export function buildLoaders(): RuleSetRule[] {
+	const stylesLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			// Creates `style` nodes from JS strings
+			"style-loader",
+			// Translates CSS into CommonJS
+			"css-loader",
+			// Compiles Sass to CSS
+			"sass-loader",
+		],
+	};
+
 	const typeScriptLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
 		exclude: /node_modules/,
-	}
+	};
 
 	return [
-		typeScriptLoader
+		typeScriptLoader,
+		stylesLoader
 	]
 }
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom'
+import './index.scss'
+
+render(
+	<div>Hello world from react</div>,
+	document.getElementById('root')
+)
