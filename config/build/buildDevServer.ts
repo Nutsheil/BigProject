5bff80a1a6eb1bf7d099ebd7faa4f3ca835ac2be
Nutshell <nutshell2088@yandex.ru>
2024-05-18 00:32:07 +0300

    add react-touter-dom and lazy loading

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: IBuildOptions): Configuration {
 	return {
 		port: options.port,
 		open: true,
+		historyApiFallback: true,
 	}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Counter } from './components/Counter'
+import { Route, Routes, Link } from 'react-router-dom'
 import './index.scss'
+import {MainPageAsync} from './pages/MainPage/MainPage.async'
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async'
+import { Suspense } from 'react'
 
 const App = () => {
 	return (
 		<div className="app">
-			<Counter />
+			<Link to={'/'} style={{marginRight: '20px'}}>Home</Link>
+			<Link to={'/About'}>About</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path='/' element={<MainPageAsync/>}/>
+					<Route path='/About'element={<AboutPageAsync/>}/>
+				</Routes>
+			</Suspense>
 		</div>
 	)
 }
