aff699c6de2bab49694c1a1a9e4ceec23ed1462a
Nutshell <nutshell2088@yandex.ru>
2024-05-19 00:01:18 +0300

    little fix to previous commit

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,8 +2,8 @@ import { Theme, useTheme } from 'app/provider/ThemeProvider';
 import { classNames } from 'shared/lib/classNames';
 import LightTheme from 'shared/assets/icons/theme-light.svg';
 import DarkTheme from 'shared/assets/icons/theme-dark.svg';
-import classes from './ThemeSwitcher.module.scss';
 import { Button } from 'shared/ui/Button'
+import classes from './ThemeSwitcher.module.scss';
 
 interface IThemeSwitcher {
 	className?: string
