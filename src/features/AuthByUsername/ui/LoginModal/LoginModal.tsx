196948b58f85e12c4b313e6b0ebbda5f88a7a71e
Nutshell <nutshell2088@yandex.ru>
2024-08-01 10:25:35 +0300

    Fix screenshots and remove LoginModal.module.scss

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,5 @@
-import { classNames } from 'shared/lib/classNames';
 import { Modal } from 'shared/ui/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import classes from './LoginModal.module.scss';
 
 interface ILoginModal {
   className?: string;
@@ -13,7 +11,7 @@ export const LoginModal = (props: ILoginModal) => {
   const { isOpen, onClose, className } = props;
 
   return (
-    <Modal className={classNames(classes.LoginModal, {}, className)} isOpen={isOpen} onClose={onClose} lazy>
+    <Modal className={className} isOpen={isOpen} onClose={onClose} lazy>
       <LoginForm />
     </Modal>
   );
