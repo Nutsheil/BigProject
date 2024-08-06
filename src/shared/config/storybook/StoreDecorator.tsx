74e9660842957b306d691118f04b0cf2c5c878fe
Nutshell <nutshell2088@yandex.ru>
2024-08-06 13:28:39 +0300

    Fix screenshots

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -2,10 +2,12 @@ import type { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import type { StoryFn } from '@storybook/react';
 import type { StateSchema } from 'app/providers/StoreProvider';
 import { StoreProvider } from 'app/providers/StoreProvider';
+import { profileReducer } from 'entities/Profile';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
 const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
   loginForm: loginReducer,
+  profile: profileReducer,
 };
 
 export const StoreDecorator = (
